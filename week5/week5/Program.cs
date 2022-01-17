ef908b65075af298281d25c55d0abe6fdb344d3b
Ikako7 <97606464+Ikako7@users.noreply.github.com>
2022-01-17 22:42:00 +0400

    add week5

---
+++ week5/week5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace week5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Irakli Sikharulidze");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Console.ReadLine());
+        }
+    }
+}
